a0166c3df5bce99506e4abf9be99b632095440bf
AbdulMoeedKhan-code <abdulmoeedkhan209@gmail.com>
2024-09-21 13:35:08 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using node js, not browser 
+
+//console.log(3
+//  +
+//  3) //code readability should be high
+
+console.log("Moeed") 
+
+let name = "Moeed"  //string
+let age = 18       //number
+let isLoggedIn = false  //booleana type
+let state;   // undefined
+let area = null
+
+
+// number => 2 to power 53
+// bigint it is used for to store big number
+// string => ""
+// boolean => true / false
+// null => standalone value 'It is the represention of empty value' 
+// undefined =>  
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);   // object
+
+
+
+
+
+
+
+
