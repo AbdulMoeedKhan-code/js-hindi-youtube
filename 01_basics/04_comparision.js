247f5599c09a8da64c867226cd0f8a0b062fabb9
AbdulMoeedKhan-code <abdulmoeedkhan209@gmail.com>
2024-09-23 09:29:19 +0000

    comparision data stype

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);   //=> ture
+// console.log("02" > 1);  //=> true
+
+// console.log(null > 0); //=>false
+// console.log(null < 0); //=>false
+// console.log(null == 0); //=>false
+// console.log(null <= 0);  //=>true
+// console.log(null >= 0);  //=>true
+
+console.log(undefined > 0);  //=> false
+console.log(undefined == 0); //=> false   'undefined is always zero wiht any value'
+console.log(undefined >= 0); //=> false     
+
+// ===  'strick check the value'  mean check the datatype of valure also
+
+console.log("2" === 2);
+
+
+
+
+
+
