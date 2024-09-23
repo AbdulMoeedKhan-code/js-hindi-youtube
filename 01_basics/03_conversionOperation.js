ecbe924ec1d6d495555c38345fe70f61db877889
AbdulMoeedKhan-code <abdulmoeedkhan209@gmail.com>
2024-09-23 07:59:42 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "moeed"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN  but his type is number 'this is confusing'
+// ture => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true;  0 => false
+// "" => false; 
+// "moeed" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
