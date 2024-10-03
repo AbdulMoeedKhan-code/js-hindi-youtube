eda563bbbbb363d281f04a51d49916bfea03e30c
AbdulMoeedKhan-code <abdulmoeedkhan209@gmail.com>
2024-10-03 05:30:33 +0000

    json talk

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -19,7 +19,182 @@ const regularUser = {
 
 // console.log(regularUser.fullname.userfullname.lastname);
 
-const obj
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+// when the user the data base the it come like an arrays of object 1st array then 2nd object 
+const users = [
+    {
+        id: 1,
+        email:"abdul@gmail.com"
+    },
+    {
+        id: 1,
+        email:"abdul@gmail.com"
+    },
+    {
+        id: 1,
+        email:"abdul@gmail.com"
+    },
+    {
+        id: 1,
+        email:"abdul@gmail.com"
+    },
+]
+
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('is LoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "moeed"
+}
+// course.courseInstructor  it find to use this syntax. => you can also use another syntax as well as following given
+
+// const {courseInstructor} = course  // in this code if you thing that the name "courseInstructor" is big name 
+                                   //   you can give change the name as given in the following syntax
+const {courseInstructor:Instructor} = course  // by using this syntax we can change the name 
+console.log(Instructor);
+
+// when we use courly braces {} this mean that we are doing destructuring
+
+// *********   API's    *********
+
+// API's are the values come form the backend and how we can write these values: => the values come in jasen these days
+
+// {
+//     "name":"moeed",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// some time we get the api's in array's from.
+
+[
+    {},
+    {},
+    {}
+]
+
+{
+    "results": [
+      {
+        "gender": "female",
+        "name": {
+          "title": "Miss",
+          "first": "Jennie",
+          "last": "Nichols"
+        },
+        "location": {
+          "street": {
+            "number": 8929,
+            "name": "Valwood Pkwy",
+          },
+          "city": "Billings",
+          "state": "Michigan",
+          "country": "United States",
+          "postcode": "63104",
+          "coordinates": {
+            "latitude": "-69.8246",
+            "longitude": "134.8719"
+          },
+          "timezone": {
+            "offset": "+9:30",
+            "description": "Adelaide, Darwin"
+          }
+        },
+        "email": "jennie.nichols@example.com",
+        "login": {
+          "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+          "username": "yellowpeacock117",
+          "password": "addison",
+          "salt": "sld1yGtd",
+          "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+          "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+          "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+        },
+        "dob": {
+          "date": "1992-03-08T15:13:16.688Z",
+          "age": 30
+        },
+        "registered": {
+          "date": "2007-07-09T05:51:59.390Z",
+          "age": 14
+        },
+        "phone": "(272) 790-0888",
+        "cell": "(489) 330-2385",
+        "id": {
+          "name": "SSN",
+          "value": "405-88-3636"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/75.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+        },
+        "nat": "US"
+      }
+    ],
+    "info": {
+      "seed": "56d27f4a53bd5441",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
